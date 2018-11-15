a1b80e3384dad8e71a631d778319288314da0651
yagawa <yagawa@qzc.co.jp>
2018-11-15 18:52:39 +0900

    Fix make array logic

--- index.js
+++ index.js
@@ -5,21 +5,22 @@
 
   APP.makeArr = function(len){
     var arr = [];
-    for(var i = 0; i < length; i++){
+    for(var i = 0; i < len; i++){
       arr.push(i + 1);
     }
     return arr;
   };
 
+
   APP.sortRandomly = function(arr){
     var random_arr = [];
-    
+
     var o = 0;
     var len = arr.length;
     var elm;
 
     for(var i = 0; i < len; i++){
-      o = Math.ceil(Math.random() * arr.length);
+      o = Math.floor(Math.random() * arr.length);
       elm = arr.splice(o, 1)[0];
       random_arr.push(elm);
     }
@@ -29,7 +30,8 @@
 
   var length = 100;
   var arr1 = APP.makeArr(length);
-  var arr2 = APP.sortRandomly(arr1); 
+  console.info(arr1);
+  var arr2 = APP.sortRandomly(arr1);
   console.info(arr2);
 
 
